8d55dafa8553b478da56379a6f8fb18fc5477cf6
Shubham Sharma <sh1nine96@gmail.com>
2022-08-08 05:41:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+//Ways to print in javascript
+// console.log("Hello World");
+// document.write("this is document write");
+// alert("This is an alert")
+
+//Javscript console API
+console.log("Hello World", 4 + 6, "Another log");
+// console.warn("this is a warning");
+// console.error("This is an error");
+
+//Java Script Variables
+//What are variables--> These are the containers to store data values
+
+var number1 = 33;
+var number2 = 44;
+//console.log(number1 + number2);
+
+//Datatypes in JavaScript
+//Numbers
+var num1 = 49; 
+var num2 = 55;
+
+//String
+
+var str1 ='This is a String';
+var str2 ="This is also a string";
+
+//Objects
+
+var marks = {
+    rajan: 38,
+    Shubham: 98,
+    Sunny: 79
+}
+console.log(marks);
+
+//Boolean
+var a = true;
+var b =false;
+console.log(a, b);
+
